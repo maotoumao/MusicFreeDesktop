22ee2af9e92598d081291f82bcabe2b7a9271c72
猫头猫 <lhx_xjtu@163.com>
2023-06-23 23:01:47 +0800

    feat: 歌词样式

--- src/renderer/components/MusicDetail/widgets/Lyric/index.tsx
+++ src/renderer/components/MusicDetail/widgets/Lyric/index.tsx
@@ -27,7 +27,11 @@ export default function Lyric() {
   }, [currentLrc?.currentLrc]);
 
   return (
-    <div className="lyric-container" data-loading={currentLrc === null} ref={containerRef}>
+    <div
+      className="lyric-container"
+      data-loading={currentLrc === null}
+      ref={containerRef}
+    >
       {
         <Condition condition={currentLrc !== null} falsy={<Loading></Loading>}>
           <Condition
