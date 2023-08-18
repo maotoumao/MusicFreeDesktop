7d2c43605ef0df298bcc841444d4dceff609d31d
猫头猫 <lhx_xjtu@163.com>
2023-08-19 01:14:59 +0800

    feat: 本地快捷键

--- src/renderer/core/shortcut/index.ts
+++ src/renderer/core/shortcut/index.ts
@@ -7,7 +7,6 @@ import Evt from "../events";
 import { shortCutKeys } from "@/common/constant";
 import rendererAppConfig from "@/common/app-config/renderer";
 
-
 const originalHotkeysFilter = hotkeys.filter;
 
 hotkeys.filter = (event) => {
@@ -29,18 +28,29 @@ const shortCutKeysEvts: Record<IShortCutKeys, keyof IEventType.IEvents> = {
   "toggle-desktop-lyric": "TOGGLE_DESKTOP_LYRIC",
 };
 
-const baseShortCutFunction = (evt: keyof IEventType.IEvents) => {
-  console.log("trigger", evt);
-  Evt.emit(evt);
+const baseShortCutFunction = (
+  evt: keyof IEventType.IEvents,
+  global: boolean
+) => {
+  if (global && rendererAppConfig.getAppConfigPath("shortCut.enableGlobal")) {
+  } else if (rendererAppConfig.getAppConfigPath("shortCut.enableLocal")) {
+    Evt.emit(evt);
+  }
 };
 
-const shortCutKeyFuncs = Object.entries(shortCutKeysEvts)
-  .map(([key, value]) => [key, baseShortCutFunction.bind(undefined, value)])
-  .reduce((prev, curr) => {
-    prev[curr[0] as string] = curr[1];
-    return prev;
-  }, {} as any);
-
+const shortCutKeyFuncs = {} as Record<any, () => void>;
+shortCutKeys.forEach((it) => {
+  shortCutKeyFuncs[it] = baseShortCutFunction.bind(
+    undefined,
+    shortCutKeysEvts[it],
+    false
+  );
+  shortCutKeyFuncs[`${it}-g`] = baseShortCutFunction.bind(
+    undefined,
+    shortCutKeysEvts[it],
+    true
+  );
+});
 
 const boundKeyMap = new Map<string, string[]>();
 export function bindShortCut(
@@ -51,8 +61,9 @@ export function bindShortCut(
   // 原有的快捷键
   const mapKey = `${eventType}${global ? "-g" : ""}`;
   unbindShortCut(eventType, global);
-  hotkeys(keys.join("+"), shortCutKeyFuncs[eventType]);
-  
+  console.log(shortCutKeyFuncs[mapKey]);
+  hotkeys(keys.join("+"), shortCutKeyFuncs[mapKey]);
+
   boundKeyMap.set(mapKey, keys);
 }
 
@@ -62,29 +73,17 @@ export function unbindShortCut(eventType: IShortCutKeys, global = false) {
 
   const originalHotKey = boundKeyMap.get(mapKey);
   if (originalHotKey) {
-    hotkeys.unbind(originalHotKey.join("+"), shortCutKeyFuncs[eventType]);
+    hotkeys.unbind(originalHotKey.join("+"), shortCutKeyFuncs[mapKey]);
     boundKeyMap.delete(mapKey);
   }
 }
 
 export function setupLocalShortCut() {
-  // window.addEventListener("keydown", (e) => {
-  //   if (e.key !== "Backspace") {
-  //     currentPressedKeys.add(characterFromEvent(e));
-  //   }
-  // });
-
-  // window.addEventListener("keyup", (e) => {
-  //   console.log("keyup", e);
-  //   currentPressedKeys.delete(characterFromEvent(e));
-  // });
-
   // 固定的快捷键
-  shortCutKeys.forEach(it => {
+  shortCutKeys.forEach((it) => {
     const val = rendererAppConfig.getAppConfigPath(`shortCut.shortcuts.${it}`);
-    if(val && val.local && val.local.length) {
-      bindShortCut(it, val.local)
+    if (val && val.local && val.local.length) {
+      bindShortCut(it, val.local);
     }
-  })
-
+  });
 }
