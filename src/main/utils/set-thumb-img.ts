378c4b9b9a58fd9e36e468f89a054ee4e5470e9e
猫头猫 <lhx_xjtu@163.com>
2023-11-05 17:03:31 +0800

    feat: 设置缩略图

--- src/main/utils/set-thumb-img.ts
+++ src/main/utils/set-thumb-img.ts
@@ -11,49 +11,57 @@ const getDefaultAlbumBuffer = asyncCacheFn(async () => {
 let hasHooked = false;
 
 export default async function (src: string, hwnd: bigint) {
-  const TaskbarThumbnailManager = (await import("@native/TaskbarThumbnailManager/TaskbarThumbnailManager.node")).default;
-  if (!hasHooked) {
-    TaskbarThumbnailManager.config(hwnd);
-    hasHooked = true;
-  }
-  let buffer: Buffer;
-  if (!src) {
-    buffer = await getDefaultAlbumBuffer();
-  } else if (src.startsWith("http")) {
-    try {
-      buffer = (
-        await axios.get(src, {
-          responseType: "arraybuffer",
-        })
-      ).data;
-    } catch {
+  try {
+    const TaskbarThumbnailManager = (
+      await import(
+        "@native/TaskbarThumbnailManager/TaskbarThumbnailManager.node"
+      )
+    ).default;
+    if (!hasHooked) {
+      TaskbarThumbnailManager.config(hwnd);
+      hasHooked = true;
+    }
+    let buffer: Buffer;
+    if (!src) {
+      buffer = await getDefaultAlbumBuffer();
+    } else if (src.startsWith("http")) {
+      try {
+        buffer = (
+          await axios.get(src, {
+            responseType: "arraybuffer",
+          })
+        ).data;
+      } catch {
+        buffer = await getDefaultAlbumBuffer();
+      }
+    } else if (src.startsWith("data:image")) {
+      buffer = Buffer.from(src.split(";base64,").pop(), "base64");
+    } else {
       buffer = await getDefaultAlbumBuffer();
     }
-  } else if (src.startsWith("data:image")) {
-    buffer = Buffer.from(src.split(";base64,").pop(), "base64");
-  } else {
-    buffer = await getDefaultAlbumBuffer();
+
+    const size = 106;
+
+    const result = await sharp(buffer)
+      .resize(size, size, {
+        fit: "cover",
+      })
+      .png()
+      .ensureAlpha(1)
+      .raw()
+      .toBuffer({
+        resolveWithObject: true,
+      });
+
+    TaskbarThumbnailManager.sendIconicRepresentation(
+      hwnd,
+      {
+        width: size,
+        height: size,
+      },
+      result.data
+    );
+  } catch {
+    console.error("Not Support");
   }
-
-  const size = 106;
-
-  const result = await sharp(buffer)
-    .resize(size, size, {
-      fit: "cover",
-    })
-    .png()
-    .ensureAlpha(1)
-    .raw()
-    .toBuffer({
-      resolveWithObject: true,
-    });
-
-  TaskbarThumbnailManager.sendIconicRepresentation(
-    hwnd,
-    {
-      width: size,
-      height: size,
-    },
-    result.data
-  );
 }
