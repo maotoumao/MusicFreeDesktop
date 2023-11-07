571d2c687156911d251d547356795de5671d5780
猫头猫 <31655147+maotoumao@users.noreply.github.com>
2023-11-07 04:47:02 +0000

    fix: #40

--- src/preload/extension.ts
+++ src/preload/extension.ts
@@ -1,12 +1,13 @@
 // See the Electron documentation for details on how to use preload scripts:
 
-import { contextBridge, ipcRenderer } from "electron";
+import { contextBridge } from "electron";
 import ipcRendererDelegate from "./internal/ipc-renderer-delegate";
 import fsDelegate from "./internal/fs-delegate";
 import themepack from "./internal/themepack";
 import path from 'path';
 import { rimraf } from "rimraf";
 import extPort from "./internal/ext-port";
+import utils from './internal/utils';
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 
 
@@ -18,3 +19,4 @@ contextBridge.exposeInMainWorld('themepack', themepack);
 contextBridge.exposeInMainWorld('path', path);
 contextBridge.exposeInMainWorld('rimraf', rimraf)
 contextBridge.exposeInMainWorld('extPort', extPort);
+contextBridge.exposeInMainWorld('utils', utils);
--- src/types/preload.d.ts
+++ src/types/preload.d.ts
@@ -4,6 +4,7 @@ interface Window {
     globalData: IGlobalData,
     path: typeof import("node:path");
     rimraf: typeof import('rimraf').rimraf;
+    utils: typeof import('../preload/internal/utils').default;
     /** 向拓展窗口广播数据 */
     mainPort: typeof import('../preload/internal/main-port').default;
     extPort: typeof import('../preload/internal/ext-port').default;
