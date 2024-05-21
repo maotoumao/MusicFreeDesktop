c8cccfdc37b1ff8afd7b4e24a212d614d09114ee
猫头猫 <lhx_xjtu@163.com>
2024-05-21 23:38:23 +0800

    feat: 升级依赖版本

--- forge.config.ts
+++ forge.config.ts
@@ -1,8 +1,6 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
-import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
-import { MakerRpm } from "@electron-forge/maker-rpm";
 import { MakerDMG } from "@electron-forge/maker-dmg";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 
