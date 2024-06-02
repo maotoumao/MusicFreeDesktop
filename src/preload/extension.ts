e3a98db0858e63ee79ba3297ac06ca5f13045323
猫头猫 <lhx_xjtu@163.com>
2024-06-02 21:06:13 +0800

    refactor: message hub

--- src/preload/extension.ts
+++ src/preload/extension.ts
@@ -1,8 +1,3 @@
-import { contextBridge } from "electron";
-// import extPort from "./internal/ext-port";
-
 import "./common-preload";
 
 import "@/shared/message-hub/preload/extension";
-
-// contextBridge.exposeInMainWorld("extPort", extPort);
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,10 +1,5 @@
 // See the Electron documentation for details on how to use preload scripts:
-import { contextBridge } from "electron";
-
-// import mainPort from "./internal/main-port";
 import "./common-preload";
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 
 import "@/shared/message-hub/preload/main";
-
-// contextBridge.exposeInMainWorld("mainPort", mainPort);
