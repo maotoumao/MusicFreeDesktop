e7963a07a98d148e6f208f870ee9924250bc8799
猫头猫 <lhx_xjtu@163.com>
2024-06-06 09:58:09 +0800

    fix: 下载失败清理源文件

--- src/webworkers/downloader.ts
+++ src/webworkers/downloader.ts
@@ -1,9 +1,22 @@
 import * as Comlink from "comlink";
 import fs from "fs";
+import fsPromises from "fs/promises";
 import { Readable } from "stream";
 import { encodeUrlHeaders } from "@/common/normalize-util";
 import throttle from "lodash.throttle";
 import { DownloadState as DownloadState } from "@/common/constant";
+import { rimraf } from "rimraf";
+
+async function cleanFile(filePath: string) {
+  try {
+    if ((await fsPromises.stat(filePath)).isFile()) {
+      await rimraf(filePath);
+    }
+    return true;
+  } catch {
+    return false;
+  }
+}
 
 const responseToReadable = (
   response: Response,
@@ -128,12 +141,18 @@ async function downloadFile(
         state,
       });
     });
+
+    stm.on("error", () => {
+      // 清理文件
+      cleanFile(filePath);
+    });
   } catch (e) {
     state = DownloadState.ERROR;
     onStateChange({
       state,
       msg: e?.message,
     });
+    cleanFile(filePath);
   }
 }
 
