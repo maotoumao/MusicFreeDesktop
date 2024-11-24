af449c6e380cd842a034c07126ff0802f4683835
maotoumao <lhx_xjtu@163.com>
2024-11-24 19:13:32 +0800

    feat: 兜底文案

--- src/renderer/components/Modal/templates/ImportMusicSheet/index.tsx
+++ src/renderer/components/Modal/templates/ImportMusicSheet/index.tsx
@@ -1,73 +1,70 @@
-import {
-  getUserPreference,
-  setUserPreference,
-} from "@/renderer/utils/user-perference";
-import { hideModal, showModal } from "../..";
+import {hideModal, showModal} from "../..";
 import Base from "../Base";
-import { ReactNode, useState } from "react";
-import Condition from "@/renderer/components/Condition";
-import Empty from "@/renderer/components/Empty";
-import { toast } from "react-toastify";
-import { useTranslation } from "react-i18next";
+import {toast} from "react-toastify";
+import {useTranslation} from "react-i18next";
 import "./index.scss";
-import { callPluginDelegateMethod } from "@/renderer/core/plugin-delegate";
+import {callPluginDelegateMethod} from "@/renderer/core/plugin-delegate";
+import NoPlugin from "@renderer/components/NoPlugin";
 
 interface IProps {
-  plugins: IPlugin.IPluginDelegate[];
+    plugins: IPlugin.IPluginDelegate[];
 }
+
 export default function ImportMusicSheet(props: IProps) {
-  const { plugins } = props;
+    const {plugins} = props;
 
-  const { t } = useTranslation();
+    const {t} = useTranslation();
 
-  return (
-    <Base withBlur={false}>
-      <div className="modal--import-music-sheet shadow backdrop-color">
-        <Base.Header>{t("plugin.method_import_music_sheet")}</Base.Header>
-        <div className="content-container">
-          {plugins.map((it) => (
-            <div
-              role="button"
-              key={it.hash}
-              className="plugin-item"
-              onClick={() => {
-                hideModal();
-                showModal("SimpleInputWithState", {
-                  title: t("plugin.method_import_music_sheet"),
-                  withLoading: true,
-                  loadingText: t("plugin_management_page.importing_media"),
-                  placeholder: t(
-                    "plugin_management_page.placeholder_import_music_sheet",
+    return (
+        <Base withBlur={false}>
+            <div className="modal--import-music-sheet shadow backdrop-color">
+                <Base.Header>{t("plugin.method_import_music_sheet")}</Base.Header>
+                <div className="content-container">
                     {
-                      plugin: it.platform,
+                        plugins?.length > 0 ? <>{plugins.map((it) => (
+                            <div
+                                role="button"
+                                key={it.hash}
+                                className="plugin-item"
+                                onClick={() => {
+                                    hideModal();
+                                    showModal("SimpleInputWithState", {
+                                        title: t("plugin.method_import_music_sheet"),
+                                        withLoading: true,
+                                        loadingText: t("plugin_management_page.importing_media"),
+                                        placeholder: t(
+                                            "plugin_management_page.placeholder_import_music_sheet",
+                                            {
+                                                plugin: it.platform,
+                                            }
+                                        ),
+                                        maxLength: 1000,
+                                        onOk(text) {
+                                            return callPluginDelegateMethod(
+                                                it,
+                                                "importMusicSheet",
+                                                text.trim()
+                                            );
+                                        },
+                                        onPromiseResolved(result) {
+                                            hideModal();
+                                            showModal("AddMusicToSheet", {
+                                                musicItems: result as IMusic.IMusicItem[],
+                                            });
+                                        },
+                                        onPromiseRejected() {
+                                            toast.error(t("plugin_management_page.import_failed"));
+                                        },
+                                        hints: it.hints?.importMusicSheet,
+                                    });
+                                }}
+                            >
+                                {it.platform}
+                            </div>
+                        ))}</> : <NoPlugin supportMethod={t("plugin.method_import_music_sheet")}></NoPlugin>
                     }
-                  ),
-                  maxLength: 1000,
-                  onOk(text) {
-                    return callPluginDelegateMethod(
-                      it,
-                      "importMusicSheet",
-                      text.trim()
-                    );
-                  },
-                  onPromiseResolved(result) {
-                    hideModal();
-                    showModal("AddMusicToSheet", {
-                      musicItems: result as IMusic.IMusicItem[],
-                    });
-                  },
-                  onPromiseRejected() {
-                    toast.error(t("plugin_management_page.import_failed"));
-                  },
-                  hints: it.hints?.importMusicSheet,
-                });
-              }}
-            >
-              {it.platform}
+                </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </Base>
-  );
+        </Base>
+    );
 }
--- src/renderer/document/index.tsx
+++ src/renderer/document/index.tsx
@@ -3,16 +3,16 @@ import App from "../app";
 import "animate.css";
 import ModalComponent from "../components/Modal";
 import bootstrap from "./bootstrap";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import MainPage from "../pages/main-page";
-import { ContextMenuComponent } from "../components/ContextMenu";
-import { ToastContainer } from "react-toastify";
+import {ContextMenuComponent} from "../components/ContextMenu";
+import {ToastContainer} from "react-toastify";
 
 import "rc-slider/assets/index.css";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css"; // 全局样式
 import "./index.scss";
-import { toastDuration } from "@/common/constant";
+import {toastDuration} from "@/common/constant";
 import useBootstrap from "./useBootstrap";
 import logger from "@shared/logger/renderer";
 
@@ -23,34 +23,34 @@ bootstrap().then(() => {
 });
 
 function Root() {
-  return (
-    <>
-      <HashRouter>
-        <BootstrapComponent></BootstrapComponent>
-        <Routes>
-          <Route path="/" element={<App></App>}>
-            <Route path="main/*" element={<MainPage></MainPage>}></Route>
-            <Route path="*" element={<MainPage></MainPage>}></Route>
-          </Route>
-        </Routes>
-      </HashRouter>
-      <ModalComponent></ModalComponent>
-      <ContextMenuComponent></ContextMenuComponent>
-      <ToastContainer
-        draggable={false}
-        closeOnClick={false}
-        limit={5}
-        pauseOnFocusLoss={false}
-        hideProgressBar
-        autoClose={toastDuration.short}
-        newestOnTop
-      ></ToastContainer>
-    </>
-  );
+    return (
+        <>
+            <HashRouter>
+                <BootstrapComponent></BootstrapComponent>
+                <Routes>
+                    <Route path="/" element={<App></App>}>
+                        <Route path="main/*" element={<MainPage></MainPage>}></Route>
+                        <Route path="*" element={<MainPage></MainPage>}></Route>
+                    </Route>
+                </Routes>
+                <ModalComponent></ModalComponent>
+            </HashRouter>
+            <ContextMenuComponent></ContextMenuComponent>
+            <ToastContainer
+                draggable={false}
+                closeOnClick={false}
+                limit={5}
+                pauseOnFocusLoss={false}
+                hideProgressBar
+                autoClose={toastDuration.short}
+                newestOnTop
+            ></ToastContainer>
+        </>
+    );
 }
 
 function BootstrapComponent(): null {
-  useBootstrap();
+    useBootstrap();
 
-  return null;
+    return null;
 }
